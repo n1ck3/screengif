dca104b7860f34cb28ac5aac1cd30211e652d3f9
Niclas Helbro <niclas.helbro@gmail.com>
2017-04-07 13:31:00 +0200

    Fixed proper argument handling. Also (kinda) fixed _only_ copy to clipboard. General other improvements.

--- screengif.py
+++ screengif.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
+"""
+Screen Gif
+
+Usage:
+    screengif.py [-d DURATION] [-i INTERVAL] [-c | --clipboard] [--debug]
+    screengif.py -h | --help
+    screengif.py -v | --version
+
+Options:
+    -h --help                           Show this screen.
+    -v --version                        Show version.
+    -i INTERVAL --interval=INTERVAL     Specify frame interval in seconds. [default: 0.1]
+    -d DURATION --duration=DURATION     Specify the duration of the GIF. [default: 5]
+    -c --clipboard                      Copy the GIF to clipboard.
+    --debug                             Show debug information.
+
+"""
+
+from docopt import docopt
 
 import os
 import sys
 import subprocess
+import shutil
 import time
 import datetime
+import tempfile
 from PIL import Image, ImageGrab
+import Clipboard
 
-# Constants in seconds
-GRAB_INTERVAL=0.1
-DURATION=1
+arguments = docopt(__doc__, version="Screen Gif 0.8")
+
+DEBUG = arguments["--debug"]
 
 def grab_screen(interval, duration):
     """
-    Grab the screen at a certain GRAB_INTERVAL.
+    Grab the screen at a set interval and for a set duration.
+
+    Returns list of images (frames).
     """
     images = []
     for i in range(int(duration/interval)):
@@ -21,46 +45,121 @@ def grab_screen(interval, duration):
         time.sleep(interval)
     return images
 
-def save_gif(images):
+def generate_gif(images, interval):
     """
-    Save the images as an animated .gif to the Desktop
-    names: "Screen Gif YYYY-MM-DD at HH.MM.SS.gif"
+    Generates the GIF from frames (saves to temporary file).
+
+    Returns temporary file pointer and its absolute path.
     """
     for im in images:
             im.thumbnail((1200,900), Image.ANTIALIAS)
 
+    tmp_fp, tmp_path = tempfile.mkstemp(suffix=".gif")
+    images[0].save(tmp_path, optimize=True, save_all=True, append_images=images[1:], duration=interval*1000*2)
+    return tmp_fp, tmp_path
+
+def save_gif(tmp_fp, tmp_path):
+    """
+    Saves the images as an animated .gif to the Desktop
+    (just like OS X default screen capture does) named:
+    "Screen Gif YYYY-MM-DD at HH.MM.SS.gif".
+
+    Closes the temp_fp file pointer (removing the temporary file).
+    """
     filename = "%s/Desktop/Screen Gif %s.gif" % (
         os.environ['HOME'],
         datetime.datetime.now().strftime("%Y-%m-%d at %H.%M.%S")
     )
-    images[0].save(filename, optimize=True, save_all=True, append_images=images[1:], duration=GRAB_INTERVAL*1000*2)
-    return filename
+
+    if DEBUG: print "Saving file to %s" % filename
+
+    shutil.copyfile(tmp_path, filename)
+    os.close(tmp_fp)
+    os.remove(tmp_path)
 
 
-def save_to_clipboard(filename):
-    command = "tell app \"Finder\" to set the clipboard to ( POSIX file \"%s\" )" % filename
+def copy_to_clipboard(tmp_fp, tmp_path):
+    """
+    Copies file to clipboard.
+
+    Closes the temp_fp file pointer (removing the temporary file).
+    """
+    if DEBUG: print "Copying file to clipboard"
+    command = "tell app \"Finder\" to set the clipboard to ( POSIX file \"%s\" )" % tmp_path
     process = subprocess.Popen(["osascript", "-e", command], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     process.communicate()
+    os.close(tmp_fp)
+
+    # TODO: Figure out a way to copy the file contents to the clipboard
+    #       rather than the file on disk. Removing the file clears the
+    #       clipboard.
+    # os.remove(tmp_path)
 
 def main():
     """
-    Doesn't do anything in itself. Just runs all the
-    other functions in the right order.
+    Handles argument parsing.
+
+    Invokes the other functions.
     """
 
-    clipboard = False
-    if len(sys.argv) > 1 and sys.argv[1] == "-c" or sys.argv[1] == "--clipboard":
-        clipboard = True
-    else:
-        print "Unrecognized option: %s" % sys.argv[1]
+
+    if DEBUG:
+        print "Printing debug information."
+        print "Arguments:"
+        print arguments
+        print
+
+    errors = []
+
+    # Handle argument: interval
+    try:
+        interval = float(arguments["--interval"])
+        interval_min = 0.1
+        interval_max = 1
+        if interval < interval_min or interval > interval_max:
+            errors.append(
+                "Interval has to be between %s and %s." % (
+                    interval_min,
+                    interval_max
+                )
+            )
+    except ValueError:
+        errors.append("Interval has to be an int of a float.")
+
+    # Handle argument: duration
+    try:
+        duration = int(arguments["--duration"])
+        duration_min = 2
+        duration_max = 10
+        if duration < duration_min or duration > duration_max:
+            errors.append(
+                "Duration has to be between %s and %s." % (
+                    duration_min,
+                    duration_max
+                )
+            )
+    except ValueError:
+        errors.append("Duration has to be an int.")
+
+    # Handle errors
+    if len(errors) > 0:
+        print "Argument Error(s):"
+        for error in errors:
+            print " > %s" % error
+        print "\nExiting..."
         sys.exit(1)
 
-    images = grab_screen(GRAB_INTERVAL, DURATION)
+    if DEBUG:
+        print "Running with parameters: interval: %s, duration: %s." % (interval, duration)
 
-    filename = save_gif(images)
+    images = grab_screen(interval, duration)
 
-    if clipboard:
-        save_to_clipboard(filename)
+    tmp_fp, tmp_path = generate_gif(images, interval)
+
+    if arguments["--clipboard"]:
+        copy_to_clipboard(tmp_fp, tmp_path)
+    else:
+        save_gif(tmp_fp, tmp_path)
 
 if __name__ == "__main__":
     main()
